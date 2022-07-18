1c1dd823fa92a92485bd7e6dd93376f4e53f4f9c
Volkan ÖZCAN <volqii@gmail.com>
2022-07-19 00:40:23 +0300

    title change

--- foo.js
+++
@@ -1,9 +0,0 @@
-console.time("fff")
-let arr = new Uint8Array(100);
-for (let i = 0; i < arr.length; i++) {
-    arr[i] = Math.random() > 0.7 ? 1 : 0;
-};
-console.timeEnd("fff")
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i].toString(2).padStart(8, "0"));
-}
