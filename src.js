af605ed3c19799c097fc7fa431cd302e8437649c
volkan <vocan86@googlemail.com>
2024-03-21 22:31:41 +0300

    enterthevoid

--- src.js
+++ src.js
@@ -1,81 +1,31 @@
-const canvas = document.getElementById('cvs');
-const ctx = canvas.getContext('2d');
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+var f = ["'East Sea Dokdo', cursive",
+    "'Gochi Hand', cursive",
+    "'Homemade Apple', cursive",
+    "'Nerko One', cursive",
+    "'Pacifico', cursive",
+    "'Permanent Marker', cursive",
+    "'Sriracha', cursive"];
+var c = ["ffbe0b", "fb5607", "ff006e", "8338ec", "3a86ff"];
+var t = document.getElementById("t");
 
-const res = 5;
-const cols = Math.floor(canvas.width / res);
-const rows = Math.floor(canvas.height / res);
-let arrDraw = [];
-let arrCalculate = [];
-const calc = (index) => {
-    let sum = 0;
-    let x = index % cols;
-    let y = Math.floor(index / cols);
-    let i = index;
-    if (x > 0) {
-        sum += arrDraw[i - 1];
-    }
-    if (x < cols - 1) {
-        sum += arrDraw[i + 1];
-    }
-    if (y > 0) {
-        sum += arrDraw[i - cols];
-    }
-    if (y < rows - 1) {
-        sum += arrDraw[i + cols];
-    }
-    if (x > 0 && y > 0) {
-        sum += arrDraw[i - cols - 1];
-    }
-    if (x > 0 && y < rows - 1) {
-        sum += arrDraw[i + cols - 1];
-    }
-    if (x < cols - 1 && y > 0) {
-        sum += arrDraw[i - cols + 1];
-    }
-    if (x < cols - 1 && y < rows - 1) {
-        sum += arrDraw[i + cols + 1];
-    }
-    return sum;
+t.addEventListener("click", function () {
+    t.style.fontFamily = f[~~(Math.random() * f.length)];
+    t.style.color = "#" + c[~~(Math.random() * c.length)];
+    //play sound 
+    var audio = new Audio('lfo.mp3');
+    audio.play();
+    audio.addEventListener('ended', function () {
+        audio.play();
+    });
+    this.innerText = "hello world!";
+    t.removeEventListener("click", arguments.callee);
+    setInterval(() => {
+        t.style.fontFamily = f[~~(Math.random() * f.length)];
+        t.style.color = "#" + c[~~(Math.random() * c.length)];
+        document.body.style.backgroundColor = "#" + c[~~(Math.random() * c.length)];
+        t.style.filter = ` hue-rotate(${Math.random() * 360}deg)`;
+
+    }, 123);
 
 }
-for (let i = 0; i < cols * rows; i++) {
-    arrDraw.push(Math.random() < 0.5 ? 1 : 0);
-}
-console.log(arrDraw)
-const init = () => {
-
-    ctx.clearRect(0, 0, innerWidth, innerHeight);
-    ctx.fillStyle = '#000';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < arrDraw.length; i++) {
-        let x = i % cols * res;
-        let y = Math.floor(i / cols) * res;
-        if (arrDraw[i]) {
-            ctx.fillStyle = '#fff';
-            ctx.fillRect(x, y, res, res);
-
-        }
-
-        if (arrDraw[i] && calc(i) < 2) {
-            arrCalculate[i] = 0;
-        } else if (arrDraw[i] && calc(i) === 2) {
-            arrCalculate[i] = 1;
-        } else if (arrDraw[i] && calc(i) === 3) {
-            arrCalculate[i] = 1;
-        } else if (arrDraw[i] && calc(i) > 3) {
-            arrCalculate[i] = 0;
-        } else if (!arrDraw[i] && calc(i) === 3) {
-            arrCalculate[i] = 1;
-        } else {
-            arrCalculate[i] = arrDraw[i];
-        }
-    }
-    arrDraw = arrCalculate;
-    arrCalculate = [];
-    requestAnimationFrame(init);
-
-}
-
-init();
+);
