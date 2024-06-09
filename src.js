0f2bc662a48edd630d13429d1efb49dc03ca6987
volkanozcan2 <vozcan86@googlemail.com>
2024-06-09 18:23:21 +0300

    css fix

--- src.js
+++ src.js
@@ -1,18 +1,18 @@
-var f = ["'East Sea Dokdo', cursive",
+let f = ["'East Sea Dokdo', cursive",
     "'Gochi Hand', cursive",
     "'Homemade Apple', cursive",
     "'Nerko One', cursive",
     "'Pacifico', cursive",
     "'Permanent Marker', cursive",
     "'Sriracha', cursive"];
-var c = ["ffbe0b", "fb5607", "ff006e", "8338ec", "3a86ff"];
-var t = document.getElementById("t");
+let c = ["ffbe0b", "fb5607", "ff006e", "8338ec", "3a86ff"];
+let t = document.getElementById("t");
 
 t.addEventListener("click", function () {
     t.style.fontFamily = f[~~(Math.random() * f.length)];
     t.style.color = "#" + c[~~(Math.random() * c.length)];
     //play sound 
-    var audio = new Audio('lfo.mp3');
+    let audio = new Audio('lfo.mp3');
     audio.play();
     audio.addEventListener('ended', function () {
         audio.pause();
