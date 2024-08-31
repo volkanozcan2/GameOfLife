2024fa7f92f6140b842195aa9249828be8c95542
volkanozcan2 <vozcan86@googlemail.com>
2024-08-31 20:05:53 +0200

    cloud shadertes

--- app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
-app.set("port", process.env.PORT || 3000);
-app.listen(app.get("port"), () => {
-    console.log(`Listening on port ${app.get("port")}`);
-});
